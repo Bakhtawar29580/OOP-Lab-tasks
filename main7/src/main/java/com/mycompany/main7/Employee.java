5a5423afe74f0fe3c125bfc5198255125b910004
Bakhtawar29580 <kbakhtawar341@gmail.com>
2025-03-19 23:17:37 +0500

    task4.2

--- main7/src/main/java/com/mycompany/main7/Employee.java
+++ main7/src/main/java/com/mycompany/main7/Employee.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.main7;
 
-/**
- *
- * @author USER
- */
 public class Employee {
+  
+    int salary = 3000;
+
+    public void calculateSalary() {
+        System.out.println("Base Salary: $" + salary);
+    }
+}
+
+class Manager extends Employee {
+    @Override
+    public void calculateSalary() {
+        int managerBaseSalary = 5000;
+        System.out.println("Manager Salary: $" + managerBaseSalary + " + Bonus");
+    }
+}
+
+class Developer extends Employee {
+    @Override
+    public void calculateSalary() {
+        int developerBaseSalary = 4000;
+        System.out.println("Developer Salary: $" + developerBaseSalary + " Project Allowance + Bonus");
+    }
     
 }
--- main7/src/main/java/com/mycompany/main7/Main7.java
+++ main7/src/main/java/com/mycompany/main7/Main7.java
@@ -1,16 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.main7;
 
-/**
- *
- * @author USER
- */
 public class Main7 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Employee e1 = new Employee();
+        e1.calculateSalary();
+
+        Manager m1 = new Manager();
+        m1.calculateSalary();
+
+        Developer d1 = new Developer();
+        d1.calculateSalary();
     }
 }
