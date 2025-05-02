6c0169f78649f0ff626bc08c213fa0c94013d296
Bakhtawar29580 <kbakhtawar341@gmail.com>
2025-05-02 11:54:50 +0500

    GUI_5

---
+++ GUI4/src/main/java/com/mycompany/gui4/GUI4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gui4;
+
+/**
+ *
+ * @author USER
+ */
+public class GUI4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
