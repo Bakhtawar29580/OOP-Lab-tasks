e86b5609653381aea20b8712abdf2a0df5654ad9
Bakhtawar29580 <kbakhtawar341@gmail.com>
2025-06-14 20:06:32 +0500

    lab 5.1

---
+++ Animall/src/main/java/com/mycompany/animall/Animall.java
@@ -0,0 +1,14 @@
+package com.mycompany.animall;
+
+public class Animall {
+
+    final void makesound(){
+        System.out.println("Animals make sounds.");
+    }
+    
+    public static void main(String[] args) {
+        Animall a1 = new Animall();
+        a1.makesound();
+        
+    }
+}
---
+++ Animall/src/main/java/com/mycompany/animall/Dog.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.animall;
+
+public class Dog extends Animall{
+    
+    @Override
+    void makesound(){
+        System.out.println("Animals make sound.");
+    }
+}
+
