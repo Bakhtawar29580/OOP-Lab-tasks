bde662f958c75f7c5d70cf6f7f0895ccf9aab32b
Bakhtawar29580 <kbakhtawar341@gmail.com>
2025-06-14 20:07:14 +0500

    lab 5.2

---
+++ Mathconst/src/main/java/com/mycompany/mathconst/Mathconst.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mathconst;
+
+final public class Mathconst {
+
+    final double E = 2.718;
+    
+    
+    public static void main(String[] args) {
+        Mathconst m1 = new Mathconst();
+        m1.E;
+        Advmath a1 = new Advmath();
+    }
+}
