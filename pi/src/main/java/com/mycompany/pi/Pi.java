8a14f45f3c406c296b0d371f05d4ea2ec859b509
Bakhtawar29580 <kbakhtawar341@gmail.com>
2025-06-16 23:49:36 +0500

    lab 6.4

---
+++ pi/src/main/java/com/mycompany/pi/Pi.java
@@ -0,0 +1,12 @@
+package com.mycompany.pi;
+
+public class Pi {
+       
+    public static void main(String[] args) {
+        
+        final double Pi = 3.14;
+        Pi= 3.1428;
+        
+        System.out.println("Value of pi is: " + Pi);
+    }
+}
